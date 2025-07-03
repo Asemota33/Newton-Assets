486123a316ab2d54fda7bb5a3f1abf7b6949c1f0
Michael Asemota <michaelasemota1@hotmail.com>
2025-07-03 01:35:50 -0400

    Added styling and form validation

--- src/app/add-assets-modal/add-assets-modal.component.ts
+++ src/app/add-assets-modal/add-assets-modal.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrashCan, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbPopover, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { AssetsService } from '../assets.service';
 import { Asset } from '../asset.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-assets-modal',
-  imports: [FontAwesomeModule, FormsModule],
+  imports: [FontAwesomeModule, FormsModule, CommonModule, NgbPopoverModule],
   templateUrl: './add-assets-modal.component.html',
   styleUrl: './add-assets-modal.component.scss'
 })
 export class AddAssetsModalComponent {
+
+  @ViewChildren('p') popoverRefs!: QueryList<NgbPopover>;
+  @ViewChildren('p1') popoverRefs1!: QueryList<NgbPopover>;
   assets: Asset[] = [{id: 1, type: '', value: null}];
   deleteBtnDisabled = true;
+  firstInvalidField: string | null = null;
+
   
   constructor(library: FaIconLibrary, public activeModal: NgbActiveModal, private assetsService: AssetsService) { 
     library.addIcons(
@@ -26,6 +32,7 @@ export class AddAssetsModalComponent {
   addAsset() {
     this.deleteBtnDisabled = false;
     this.assets.push({id: this.assets.length + 1, type: '', value: null});
+
   }
 
   deleteAsset(index: number) {
@@ -38,8 +45,33 @@ export class AddAssetsModalComponent {
   }
 
   submitAssets() {
-    this.assetsService.addAsset(this.assets)
-    this.activeModal.close(this.assetsService.getAssets());
+    setTimeout(() => {
+      const forms = document.querySelectorAll('form');
+      for (let i = 0; i < forms.length; i++) {
+        const invalidInput = forms[i].querySelector('.is-invalid');
+        if (invalidInput) {
+          (invalidInput as HTMLElement).focus();
+          let inputField = invalidInput.tagName.startsWith('SELECT') ? 'type' : 'value';
+
+          if (inputField === 'type') {
+            const popoverDirective = this.popoverRefs.find((ref, idx) => idx === Number(invalidInput.getAttribute('data-index')));
+            popoverDirective?.open();
+            setTimeout(() => popoverDirective?.close(), 2000);
+          }
+          if (inputField === 'value') {
+            const secondPopoverDirective = this.popoverRefs1.find((ref, idx) => idx === Number(invalidInput.getAttribute('data-index')));
+            secondPopoverDirective?.open();
+            setTimeout(() => secondPopoverDirective?.close(), 2000);
+          }
+
+          break;
+        }
+        else {
+          this.assetsService.addAsset(this.assets)
+          this.activeModal.close(this.assetsService.getAssets());
+        }
+      }
+    });
 
   }
 
@@ -47,8 +79,8 @@ export class AddAssetsModalComponent {
     if (asset.value !== null && asset.value !== undefined && asset.value !== '') {
       asset.value = parseFloat(asset.value).toFixed(2);
     }
-}
-
+  }
+  
   closeModal() {
     this.activeModal.close();
   }
