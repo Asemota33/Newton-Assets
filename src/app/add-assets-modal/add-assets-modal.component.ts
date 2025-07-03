086ded6517c3f59610247de3fc7a029c310565ce
Michael Asemota <michaelasemota1@hotmail.com>
2025-07-03 02:14:55 -0400

    Removed popover

--- src/app/add-assets-modal/add-assets-modal.component.ts
+++ src/app/add-assets-modal/add-assets-modal.component.ts
@@ -1,7 +1,7 @@
-import { Component, QueryList, ViewChildren } from '@angular/core';
+import { Component } from '@angular/core';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrashCan, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import { NgbActiveModal, NgbPopover, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { AssetsService } from '../assets.service';
 import { Asset } from '../asset.interface';
@@ -9,14 +9,12 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-assets-modal',
-  imports: [FontAwesomeModule, FormsModule, CommonModule, NgbPopoverModule],
+  imports: [FontAwesomeModule, FormsModule, CommonModule ],
   templateUrl: './add-assets-modal.component.html',
   styleUrl: './add-assets-modal.component.scss'
 })
 export class AddAssetsModalComponent {
 
-  @ViewChildren('p') popoverRefs!: QueryList<NgbPopover>;
-  @ViewChildren('p1') popoverRefs1!: QueryList<NgbPopover>;
   assets: Asset[] = [{id: 1, type: '', value: null}];
   deleteBtnDisabled = true;
   firstInvalidField: string | null = null;
@@ -51,19 +49,6 @@ export class AddAssetsModalComponent {
         const invalidInput = forms[i].querySelector('.is-invalid');
         if (invalidInput) {
           (invalidInput as HTMLElement).focus();
-          let inputField = invalidInput.tagName.startsWith('SELECT') ? 'type' : 'value';
-
-          if (inputField === 'type') {
-            const popoverDirective = this.popoverRefs.find((ref, idx) => idx === Number(invalidInput.getAttribute('data-index')));
-            popoverDirective?.open();
-            setTimeout(() => popoverDirective?.close(), 2000);
-          }
-          if (inputField === 'value') {
-            const secondPopoverDirective = this.popoverRefs1.find((ref, idx) => idx === Number(invalidInput.getAttribute('data-index')));
-            secondPopoverDirective?.open();
-            setTimeout(() => secondPopoverDirective?.close(), 2000);
-          }
-
           break;
         }
         else {
